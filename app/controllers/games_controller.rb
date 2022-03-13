0eb01fc04c89924cdd44c64a4641892fc14b25d1
nittamatama <blackmnshin@gmail.com>
2022-03-13 14:44:54 +0900

    correct画面生成

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,27 @@ class GamesController < ApplicationController
     redirect_to new_game_progresses_path(game)
   end
 
+  def update
+    current_game = Game.find(params[:id])
+
+    if params[:correct].present?
+      current_game.status = 'finished'
+      current_game.result = :correct
+      current_game.save!
+
+      redirect_to correct_game_path(current_game)
+    else
+      current_game.status = 'finished'
+      current_game.result = :incorrect
+      current_game.save!
+
+      redirect_to give_up_game_path(current_game)
+    end
+  end
+
+  def correct
+  end
+
   def challenge
     current_game = Game.find(params[:id])
     extract_comics =  ExtractionAlgorithm.new(current_game).compute
